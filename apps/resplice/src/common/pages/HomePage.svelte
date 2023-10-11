52538549073d75b5b62911ff2cc70b9425c46beb
Marcus Virginia <marcusvirg345@gmail.com>
2023-10-11 00:09:03 -0500

    remove party invite from app

--- apps/resplice/src/common/pages/HomePage.svelte
+++ apps/resplice/src/common/pages/HomePage.svelte
@@ -3,18 +3,16 @@
 	import cx from 'clsx'
 	import accountStore from '$modules/account/account.store'
 	import NavItem from '$common/components/NavItem.svelte'
-	import { Avatar, BalloonIcon } from '@resplice/components'
-	// import NavActions from '$common/components/NavActions.svelte'
-	import PartyShare from '$common/components/PartyShare.svelte'
-	// import ConnectionListPage from '$modules/connection/pages/ConnectionListPage.svelte'
-	import LaunchPartyInvite from '$common/components/LaunchPartyInvite.svelte'
+	import { Avatar, PeopleIcon } from '@resplice/components'
+	import NavActions from '$common/components/NavActions.svelte'
+	import ConnectionListPage from '$modules/connection/pages/ConnectionListPage.svelte'
 	import ProfilePage from '$modules/account/pages/ProfilePage.svelte'
 
 	$: isOnContacts = $location.includes('/home/party')
 	$: isOnProfile = $location.includes('/home/profile')
 
 	const routes = {
-		'/home/party': LaunchPartyInvite,
+		'/home/connections': ConnectionListPage,
 		'/home/profile': ProfilePage
 	}
 </script>
@@ -27,11 +25,10 @@
 		class="relative flex-none flex justify-around items-center p-2 border-t-2 border-gray-200 bg-white overflow-hidden"
 		style="padding-bottom: calc(env(safe-area-inset-bottom) + 0.5rem)"
 	>
-		<NavItem isActive={isOnContacts} href="#/home/party">
-			<BalloonIcon width={32} height={32} />
+		<NavItem isActive={isOnContacts} href="#/home/connections">
+			<PeopleIcon width={32} height={32} />
 		</NavItem>
-		<PartyShare />
-		<!-- <NavActions /> -->
+		<NavActions />
 		<button
 			class={cx(
 				'w-12 h-12 flex items-center justify-center rounded-full no-highlight transform transition duration-75 ease-in-out active:scale-90',
@@ -42,8 +39,8 @@
 			)}
 			on:click={() => push('/home/profile')}
 		>
-			<Avatar size="sm" seed={$accountStore.uuid} src={$accountStore.avatarUrl} /></button
-		>
+			<Avatar size="sm" seed={$accountStore.uuid} src={$accountStore.avatarUrl} />
+		</button>
 		<!-- <NavItem isActive={isOnChats} href="#/home/chats">
 			<ChatBubblesIcon width={32} height={32} />
 		</NavItem> -->
